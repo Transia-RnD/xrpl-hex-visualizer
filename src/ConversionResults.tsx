ab2234c8a958aa4907781f3b072fe636bdc8ccaf
Denis Angell <dangell@transia.co>
2024-08-22 21:04:01 +0200

    add c array

--- src/ConversionResults.tsx
+++ src/ConversionResults.tsx
@@ -5,14 +5,47 @@ export type ConversionResultsProps = {
   results: Record<string, any>;
 };
 
+const carrayList = [
+  'namespace',
+  'hash',
+  'xrpCurrency',
+  'xrpAddress'
+]
+
+const createCArray = (value: string) => {
+  // split the string into bytes and return as c array 0x00, 0x01, 0x02, ...
+  // uint8_t array[] = {0x00, 0x01, 0x02, ...};
+  const bytes = value.match(/.{1,2}/g);
+  if (bytes) {
+    return `uint8_t array[] = {${bytes.map((byte) => `0x${byte}`).join(', ')}};`;
+  }
+}
+
 const ConversionResults: React.FC<ConversionResultsProps> = ({ results }) => {
   const resultMap = Object.entries(results).map(([type, value]) => {
     // Exclude string if there are other valid results
+    if (type === 'xrpCurrency' && value.length !== 40) {
+      return null
+    }
     if (value !== 'Invalid size' && value !== '') {
       if (type === 'xflDecimal') {
         return <p key={type}>{`${type} (uint64): ${value}`}</p>;
       } else {
-        return <p key={type}>{`${type}: ${value}`}</p>;
+        return <p key={type}>
+          {`${type}: ${value}`}
+          {carrayList.includes(type) && (
+            <div>
+              <p>{`${type} array: `}</p>
+              <textarea
+                value={createCArray(value)}
+                placeholder={`${type} array: `}
+                rows={2}
+                cols={110}
+                style={{ fontFamily: 'monospace' }}
+              />
+            </div>
+          )}
+        </p>;
       }
     }
     return null; // or you could return a placeholder if you want to show something for invalid sizes
